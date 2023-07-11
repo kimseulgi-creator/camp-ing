8514f4820093fc53d9b30d59ae40b92f75fd083a
seulgi <rlatmfrl9@naver.com>
2023-07-11 18:03:39 +0900

    Feat: 로그인, 로그아웃 구현 로그인하게 되면 해당 user db에 isLogin value가 true, 로그아웃 버튼을 누르면 false로 값이 바뀜.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,17 +2,51 @@ import React from 'react';
 import { styled } from 'styled-components';
 import logo from '../images/header_logo.svg';
 import { StButton } from '../pages/Home';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { editUser, getUsers } from '../api/users';
+import { useNavigate } from 'react-router-dom';
 
 function Layout(props) {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const mutation = useMutation(editUser, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users');
+      console.log('성공하였습니다!');
+    },
+  });
+
+  const { isLoading, isError, data } = useQuery('users', getUsers);
+  if (isLoading) {
+    return <p>로딩중입니다...</p>;
+  }
+  const filterLoginUser = data.filter((user) => {
+    return user.isLogin === true;
+  });
+  const [loginUser] = filterLoginUser;
+  console.log(loginUser);
+
+  const logoutButtonHandler = () => {
+    const { id, isLogin } = loginUser;
+    mutation.mutate({ id, isLogin });
+  };
+
   return (
     <StBackground>
       <StHeader>
-        <p>paperwhite_wits</p>
+        <a className="loginUserId">{loginUser.userId}</a>
         <h1>
           <a href="#">camp ing</a>
         </h1>
         <StButton>
-          <button>Logout</button>
+          <button
+            onClick={function () {
+              logoutButtonHandler();
+              navigate('/');
+            }}
+          >
+            Logout
+          </button>
         </StButton>
       </StHeader>
       <StInner>{props.children}</StInner>
@@ -20,7 +54,7 @@ function Layout(props) {
   );
 }
 
-export default Layout;
+export default React.memo(Layout);
 const StBackground = styled.div`
   background-color: #2b8279;
 `;
@@ -35,8 +69,18 @@ const StHeader = styled.header`
   overflow: hidden;
   width: 100%;
   z-index: 2;
-  & p {
-    font-size: 15px;
+  & .loginUserId {
+    font-size: 14px;
+    font-weight: bold;
+    width: 100px;
+    line-height: 30px;
+    text-align: center;
+    cursor: pointer;
+    transition: all 1s;
+    border-radius: 30px;
+  }
+  & .loginUserId:hover {
+    box-shadow: 2px 2px 5px 0 #00000030;
   }
   & h1 > a {
     background-image: url(${logo});
